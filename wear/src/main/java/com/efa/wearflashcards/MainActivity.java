575a0d24815ecd000d4758d774b8adf01351085a
Eric Abreu <abreu@college.harvard.edu>
2015-12-05 15:39:55 -0500

    Add wearable list view example from the Android Developers website

--- wear/src/main/java/com/efa/wearflashcards/MainActivity.java
+++ wear/src/main/java/com/efa/wearflashcards/MainActivity.java
@@ -1,24 +1,92 @@
 package com.efa.wearflashcards;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableListView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+    private static MainActivity instance;
     private TextView mTextView;
+
+    public static Context getContext() {
+        return instance.getApplicationContext();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        instance = this;
+        String[] elements = {"List Item 1", "List Item 2"};
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+
+        // Get the list component from the layout of the activity
+        WearableListView listView =
+                (WearableListView) findViewById(R.id.list);
+
+        // Assign an adapter to the list
+        listView.setAdapter(new Adapter(this, elements));
+    }
+
+    /**
+     * http://developer.android.com/training/wearables/ui/lists.html
+     */
+    private static final class Adapter extends WearableListView.Adapter {
+        private final Context mContext;
+        private final LayoutInflater mInflater;
+        private String[] mDataset;
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public Adapter(Context context, String[] dataset) {
+            mContext = context;
+            mInflater = LayoutInflater.from(context);
+            mDataset = dataset;
+        }
+
+        // Create new views for list items
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                              int viewType) {
+            // Inflate our custom layout for list items
+            return new ItemViewHolder(mInflater.inflate(R.layout.set_list_item, null));
+        }
+
+        // Replace the contents of a list item
+        // Instead of creating new views, the list tries to recycle existing ones
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public void onBindViewHolder(WearableListView.ViewHolder holder,
+                                     int position) {
+            // retrieve the text view
+            ItemViewHolder itemHolder = (ItemViewHolder) holder;
+            TextView view = itemHolder.textView;
+            // replace text contents
+            view.setText(mDataset[position]);
+            // replace list item's metadata
+            holder.itemView.setTag(position);
+        }
+
+        // Return the size of your dataset
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+
+        // Provide a reference to the type of views you're using
+        public static class ItemViewHolder extends WearableListView.ViewHolder {
+            private TextView textView;
+
+            public ItemViewHolder(View itemView) {
+                super(itemView);
+                // find the text view within the custom item's layout
+                textView = (TextView) itemView.findViewById(R.id.name);
             }
-        });
+        }
     }
 }
---
+++ wear/src/main/java/com/efa/wearflashcards/data/FlashcardContract.java
@@ -0,0 +1,68 @@
+package com.efa.wearflashcards.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Defines table and column names for database.
+ */
+public final class FlashcardContract {
+
+    // URI variables
+    public static final String AUTHORITY = "com.efa.wearflashcards.app";
+    public static final Uri BASE_URI = Uri.parse("content://" + AUTHORITY);
+
+    // Empty constructor to prevent someone from accidentally instantiating the contract class
+    public FlashcardContract() {
+    }
+
+    // Table with all flashcard sets
+    public static abstract class SetList implements BaseColumns {
+        // Table directory
+        public static final String TABLE_DIR = "set_list";
+
+        // Content URI for table
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_URI, TABLE_DIR);
+
+        // Mime type of a directory of sets
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                AUTHORITY + "/" + TABLE_DIR;
+
+        // Mime type of a single set
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" +
+                AUTHORITY + "/" + TABLE_DIR;
+
+        // Columns in table
+        public static final String TABLE_NAME = "sets";
+        public static final String SET_TABLE_NAME = "set_table_name";
+        public static final String SET_TITLE = "set_title";
+
+        // Default sort order
+        public static final String SORT_ORDER_DEFAULT = "_ID ASC";
+    }
+
+    // Table with a single set of flashcards
+    public static abstract class CardSet implements BaseColumns {
+        // Table directory
+        public static final String TABLE_DIR = "card_sets";
+
+        // Content URI for table
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_URI, TABLE_DIR);
+
+        // Mime type of a set of cards
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                AUTHORITY + "/" + TABLE_DIR;
+
+        // Mime type of a single card
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" +
+                AUTHORITY + "/" + TABLE_DIR;
+
+        // Columns in table
+        public static final String TERM = "term";
+        public static final String DEFINITION = "definition";
+
+        // Default sort order
+        public static final String SORT_ORDER_DEFAULT = "_ID ASC";
+    }
+}
