3fc0887430c632bbec8d95305b268f9e627f6f89
Eric Abreu <abreu@college.harvard.edu>
2015-12-07 13:28:32 -0500

    Update wearable card style

--- wear/src/main/java/com/efa/wearflashcards/SetView.java
+++ wear/src/main/java/com/efa/wearflashcards/SetView.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.wearable.view.CardFragment;
+import android.view.Gravity;
 
 public class SetView extends Activity {
 
@@ -16,8 +17,10 @@ public class SetView extends Activity {
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         CardFragment cardFragment = CardFragment.create(getString(R.string.term),
-                getString(R.string.definition),
-                R.drawable.wl_circle);
+                getString(R.string.definition));
+        cardFragment.setCardGravity(Gravity.TOP);
+        cardFragment.setExpansionDirection(CardFragment.EXPAND_DOWN);
+        cardFragment.setExpansionEnabled(true);
         fragmentTransaction.add(R.id.frame_layout, cardFragment);
         fragmentTransaction.commit();
     }
